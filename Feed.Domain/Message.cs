bb28363c7d383c7b2e304b9ca30345e07f915fb5
Александр <117838226+TIKI-developer@users.noreply.github.com>
2026-06-15 11:48:51 +0800

    Update Message.cs

--- Feed.Domain/Message.cs
+++ Feed.Domain/Message.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace Feed.Domain
+{
+    public class Message
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Body { get; private set; }
+        public string Source { get; private set; }
+
+        private Message() { }
+    }
+}
