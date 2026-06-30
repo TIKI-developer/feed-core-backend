5554a101d56a44642b9b139389efc1f94fa9de44
Александр <117838226+TIKI-developer@users.noreply.github.com>
2026-06-30 21:01:52 +0800

    Created Feed.Security
    - Implemented PasswordHasher (hashing and verifying strings)
    - DependecyInjection configuration

---
+++ Feed.Security/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Feed.Domain.Shared.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Feed.Security;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSecurity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IStringHasher, PasswordHasher>();
+        services.AddScoped<IHashVerifier, PasswordHasher>();
+
+        return services;
+    }
+}
---
+++ Feed.Security/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using Feed.Domain.Shared.Interfaces;
+
+namespace Feed.Security;
+
+public class PasswordHasher : IStringHasher, IHashVerifier
+{
+    public string ComputeHash(string raw)
+    {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(raw);
+    }
+
+    public bool Verify(string raw, string hash)
+    {
+        return BCrypt.Net.BCrypt.EnhancedVerify(raw, hash);
+    }
+}
