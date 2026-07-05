58f412ae2c091fcdaf73677b0f5de170e45a9802
Александр <117838226+TIKI-developer@users.noreply.github.com>
2026-07-05 17:35:55 +0800

    Implemented WebApi Layer for DictionaryCreate
    Added:
    - DictionaryController.cs
    - CreateDictionaryRequestMapping.cs
    - CreateDictionaryResponseMapping.cs
    - CreateDictionaryRequest.cs
    - CreateDictionaryResponse.cs

---
+++ src/Feed.WebApi/Mappings/CreateDictionaryRequestMapping.cs
@@ -0,0 +1,15 @@
+﻿using Feed.Application.UseCases.Dictionaries.Commands.CreateDictionar;
+using Feed.WebApi.Requests;
+
+namespace Feed.WebApi.Mappings;
+
+public static class CreateDictionaryRequestMapping
+{
+    public static CreateDictionaryCommand ToCommand(this CreateDictionaryRequest request)
+    {
+        return new CreateDictionaryCommand
+        {
+            Name = request.Name
+        };
+    }
+}
---
+++ src/Feed.WebApi/Responses/CreateDictionaryResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Feed.WebApi.Responses;
+
+public readonly record struct CreateDictionaryResponse
+{
+    public required Guid Id { get; init; }
+}
