76620aeecff89af035a135158168e7d2337f3aaa
Александр <117838226+TIKI-developer@users.noreply.github.com>
2026-07-05 17:27:47 +0800

    Create IDictionaryRepository.cs

---
+++ src/Feed.Application/Interfaces/IDictionaryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Feed.Domain.Dictionaries.Entities;
+
+namespace Feed.Application.Interfaces;
+
+public interface IDictionaryRepository : IBaseRepository
+{
+    Task AddAsync(Dictionary dictionary, CancellationToken cancellationToken = default);
+}
