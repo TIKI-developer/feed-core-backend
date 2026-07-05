a19f8a02fd3f918b87c67fabf77ff9ff400ae3ac
Александр <117838226+TIKI-developer@users.noreply.github.com>
2026-07-05 17:29:20 +0800

    Added CreateDictionary UseCase
    Added:
    - Command
    - CommandHandler
    - CommandResult

---
+++ src/Feed.Application/UseCases/Dictionaries/Commands/CreateDictionary/CreateDictionaryCommandResult.cs
@@ -0,0 +1,6 @@
+﻿namespace Feed.Application.UseCases.Dictionaries.Commands.CreateDictionary;
+
+public readonly record struct CreateDictionaryCommandResult
+{
+    public required Guid Id { get; init; }
+}
