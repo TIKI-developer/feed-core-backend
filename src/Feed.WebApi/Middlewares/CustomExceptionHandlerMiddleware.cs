11c286cf52a9d1dd0c768a87ca47ac89e8bea9fa
Александр <117838226+TIKI-developer@users.noreply.github.com>
2026-07-16 04:43:36 +0800

    Added middleware exception handler

---
+++ src/Feed.WebApi/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Feed.Application.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace Feed.WebApi.Middlewares;
+
+public class CustomExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CustomExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError;
+        var result = string.Empty;
+        switch (exception)
+        {
+            case NotFoundException:
+                code = HttpStatusCode.NotFound;
+                break;
+            case AlreadyExist:
+                code = HttpStatusCode.Conflict;
+                break;
+        }
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+
+        if (result == string.Empty)
+        {
+            result = JsonSerializer.Serialize(new { error = exception.Message });
+        }
+
+        return context.Response.WriteAsync(result);
+    }
+}
